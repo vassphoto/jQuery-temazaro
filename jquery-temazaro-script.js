a0b8cde490f1454fd7955d76cd9f16fd8c0bf4c4
Zoltan Vass <103262012+vassphoto@users.noreply.github.com>
2022-04-13 13:58:38 +0200

    Végleges verzió

--- jquery-temazaro-script.js
+++ jquery-temazaro-script.js
@@ -1,42 +1,45 @@
-// function clear(){
-//     document.getElementById('kijelzo').value = ''
-//     document.getElementById("kijelzo").innerHTML = torles;
-// }
-
-// function eredmeny(kattintva){
-//     document.getElementById('kijelzo').value += kattintva
-//     return kattintva
-// }
-
-// function calculate(){
-//     let x = document.getElementById('kijelzo').value
-//     let y = eval(x)
-//     document.getElementById('kijelzo').value = y       
-//     return y    
-// }
-
-const buttons = document.getElementsByTagName('button');
-
-for (let i = 0; i < buttons.length; i++){
-    buttons[i].onclick = function () {
-        const kattintva = buttons[i].innerText;
-
-        if (kattintva == 'C') {
-            //törlés
-            document.getElementById("kijelzo").value = ''; 
-        }
-        
-        else if (kattintva == '=') {
-            //kiszámol
-            document.getElementById("kijelzo").value = eval (document.getElementById("kijelzo").value);
-            
-        }
-
-        else {
-            //hozzáfűzés
-            document.getElementById("kijelzo").value += buttons[i].innerHTML;
-        }
-    }
-}
-
-
+$(function(){
+	var form = $("form");
+	
+	form.on("submit", function(e){
+		e.preventDefault();
+		var resultDiv = $(".result .title");
+		
+		function calcBMI(){
+			var weight = $("#weight").val();
+	    var height = $("#height").val();
+			var bmi = weight / ((height / 100) * (height / 100));
+			return bmi.toFixed(2);
+			
+		}
+		
+		function bmiState(){
+			if(calcBMI() < 15.99 ){
+				return "Súlyos soványság";
+			}
+			if(calcBMI() > 16 && calcBMI() < 16.99 ){
+				return "Mérsékelt soványság";
+			}
+			if(calcBMI() > 17 && calcBMI() < 18.49 ){
+				return "Enyhe soványság";
+			}
+			if(calcBMI() > 18.5 && calcBMI() < 24.99 ){
+				return "Normális testsúly";
+			}
+			if(calcBMI() > 25 && calcBMI() < 29.99 ){
+				return "Túlsúlyos";
+			}
+			if(calcBMI() > 30 && calcBMI() < 34.99 ){
+				return "I. fokú elhízás";
+			}
+            if(calcBMI() > 35 && calcBMI() < 39.99 ){
+				return "II. fokú elhízás";
+			}
+			if(calcBMI() > 40){
+				return "III. fokú elhízás";
+			}
+		}
+		
+		resultDiv.html(" TTI értéked: " + calcBMI() + " -> " + bmiState());
+	});
+});
